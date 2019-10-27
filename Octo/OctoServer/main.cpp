deeff5f9ec5499acaa8abb43e715ae063f70f01a
fvolkanoski <f.volkanoski@gmail.com>
2019-10-27 23:59:05 +0100

    Implemented ImGUI and moved the server and client to separate threads

--- Octo/OctoServer/main.cpp
+++ Octo/OctoServer/main.cpp
@@ -1,68 +1,100 @@
-//
-// sender.cpp
-// ~~~~~~~~~~
-//
-// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+// Boost.Asio initialization header.
+#include "asioinit.h"
 
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <boost/asio.hpp>
+// ImGUI initialization header.
+#include "guiinit.h"
 
-constexpr short multicast_port = 30001;
-constexpr int max_message_count = 90;
+// Constants header.
+#include "constants.h"
 
-class sender
+// Std includes.
+#include <thread>
+
+bool CreateDeviceD3D(HWND hWnd)
 {
-public:
-	sender(boost::asio::io_context& io_context,
-		const boost::asio::ip::address& multicast_address)
-		: endpoint_(multicast_address, multicast_port),
-		socket_(io_context, endpoint_.protocol()),
-		timer_(io_context),
-		message_count_(0)
+	// Setup the swap chain.
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 2;
+	sd.BufferDesc.Width = 0;
+	sd.BufferDesc.Height = 0;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	UINT createDeviceFlags = 0;
+	D3D_FEATURE_LEVEL featureLevel;
+
+	const D3D_FEATURE_LEVEL featureLevelArray[2] = 
+	{ 
+		D3D_FEATURE_LEVEL_11_0, 
+		D3D_FEATURE_LEVEL_10_0, 
+	};
+
+	if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray,
+		2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
 	{
-		do_send();
+		return false;
 	}
 
-private:
-	void do_send()
+	CreateRenderTarget();
+	return true;
+}
+
+void CleanupDeviceD3D()
+{
+	CleanupRenderTarget();
+	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+void CreateRenderTarget()
+{
+	ID3D11Texture2D* pBackBuffer;
+	g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+	pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+}
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	switch (msg)
 	{
-		std::ostringstream os;
-		os << "Message " << message_count_++;
-		message_ = os.str();
-
-		socket_.async_send_to(
-			boost::asio::buffer(message_), endpoint_,
-			[this](boost::system::error_code ec, std::size_t /*length*/)
-			{
-				if (!ec && message_count_ < max_message_count)
-					do_timeout();
-			});
+	case WM_SIZE:
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			CleanupRenderTarget();
+			g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+			CreateRenderTarget();
+		}
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return 0;
+		break;
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		return 0;
 	}
-
-	void do_timeout()
-	{
-		timer_.expires_after(std::chrono::seconds(1));
-		timer_.async_wait(
-			[this](boost::system::error_code ec)
-			{
-				if (!ec)
-					do_send();
-			});
-	}
-
-private:
-	boost::asio::ip::udp::endpoint endpoint_;
-	boost::asio::ip::udp::socket socket_;
-	boost::asio::steady_timer timer_;
-	int message_count_;
-	std::string message_;
-};
+	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int main(int argc, char* argv[])
 {
@@ -78,14 +110,89 @@ int main(int argc, char* argv[])
 			return 1;
 		}
 
-		boost::asio::io_context io_context;
-		sender s(io_context, boost::asio::ip::make_address(argv[1]));
-		io_context.run();
+		std::thread serverThread(runOctoServer, argv);
+
+		// Hide the console window, since we are gonna create a window for ImGUI.
+		//::ShowWindow(::GetConsoleWindow(), SW_HIDE);
+
+		// Create the new application window.
+		WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
+		::RegisterClassEx(&wc);
+		HWND hwnd = ::CreateWindow(wc.lpszClassName, _T(windowName), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+
+		// Initialize Direct3D.
+		if (!CreateDeviceD3D(hwnd))
+		{
+			CleanupDeviceD3D();
+			::UnregisterClass(wc.lpszClassName, wc.hInstance);
+			return 1;
+		}
+
+		// Show the DirectX window.
+		::ShowWindow(hwnd, SW_SHOWDEFAULT);
+		::UpdateWindow(hwnd);
+
+		// Setup the ImGUI context.
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+		// Setup the ImGUI style.
+		ImGui::StyleColorsClassic();
+
+		// Setup Platform/Renderer bindings.
+		ImGui_ImplWin32_Init(hwnd);
+		ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+		// Loop variables.
+		MSG msg;
+		ZeroMemory(&msg, sizeof(msg));
+
+		// The main render loop.
+		while (msg.message != WM_QUIT)
+		{
+			if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+			{
+				::TranslateMessage(&msg);
+				::DispatchMessage(&msg);
+				continue;
+			}
+
+			// Start the ImGUI frame.
+			ImGui_ImplDX11_NewFrame();
+			ImGui_ImplWin32_NewFrame();
+			ImGui::NewFrame();
+
+			ImGui::Begin("Hello, world!");
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+
+			// Render the windows created.
+			ImGui::Render();
+			g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+			g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, windowBackground);
+			ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+			// Present with vsync, set first input variable to 0 for without vsync.
+			g_pSwapChain->Present(1, 0);
+		}
+
+		// Do the cleanup before shutting down the application.
+		ImGui_ImplDX11_Shutdown();
+		ImGui_ImplWin32_Shutdown();
+		ImGui::DestroyContext();
+		CleanupDeviceD3D();
+		::DestroyWindow(hwnd);
+		::UnregisterClass(wc.lpszClassName, wc.hInstance);
+		
+		// Detach the server thread, to be able to call the destructor to terminate the thread before exiting.
+		serverThread.detach();
+		serverThread.~thread();
+
+		return 0;
 	}
 	catch (std::exception & e)
 	{
 		std::cerr << "Exception: " << e.what() << "\n";
 	}
-
-	return 0;
 }
